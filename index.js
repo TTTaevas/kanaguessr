7374d66e33beabddd4f5cd27efe170d25e7da70b
Taevas <code@taevas.xyz>
2025-02-24 22:21:58 +0100

    Refresh on settings change & keep track of right answers
    If you were able to write the character in another syllabary
    kanaguessr will not show it again until you've got all characters basically

--- index.js
+++ index.js
@@ -1,17 +1,333 @@
+const items = [
+	{
+		"h": "あ",
+		"k": "ア",
+		"r": "a"
+	},
+	{
+		"h": "い",
+		"k": "イ",
+		"r": "i"
+	},
+	{
+		"h": "う",
+		"k": "ウ",
+		"r": "u"
+	},
+	{
+		"h": "え",
+		"k": "エ",
+		"r": "e"
+	},
+	{
+		"h": "お",
+		"k": "オ",
+		"r": "o"
+	},
+	{
+		"h": "か",
+		"k": "カ",
+		"r": "ka"
+	},
+	{
+		"h": "き",
+		"k": "キ",
+		"r": "ki"
+	},
+	{
+		"h": "く",
+		"k": "ク",
+		"r": "ku"
+	},
+	{
+		"h": "け",
+		"k": "ケ",
+		"r": "ke"
+	},
+	{
+		"h": "こ",
+		"k": "コ",
+		"r": "ko"
+	},
+	{
+		"h": "さ",
+		"k": "サ",
+		"r": "sa"
+	},
+	{
+		"h": "し",
+		"k": "シ",
+		"r": "shi"
+	},
+	{
+		"h": "す",
+		"k": "ス",
+		"r": "su"
+	},
+	{
+		"h": "せ",
+		"k": "セ",
+		"r": "se"
+	},
+	{
+		"h": "そ",
+		"k": "ソ",
+		"r": "so"
+	},
+	{
+		"h": "た",
+		"k": "タ",
+		"r": "ta"
+	},
+	{
+		"h": "ち",
+		"k": "チ",
+		"r": "chi"
+	},
+	{
+		"h": "つ",
+		"k": "ツ",
+		"r": "tsu"
+	},
+	{
+		"h": "て",
+		"k": "テ",
+		"r": "te"
+	},
+	{
+		"h": "と",
+		"k": "ト",
+		"r": "to"
+	},
+	{
+		"h": "な",
+		"k": "ナ",
+		"r": "na"
+	},
+	{
+		"h": "に",
+		"k": "ニ",
+		"r": "ni"
+	},
+	{
+		"h": "ぬ",
+		"k": "ヌ",
+		"r": "nu"
+	},
+	{
+		"h": "ね",
+		"k": "ネ",
+		"r": "ne"
+	},
+	{
+		"h": "の",
+		"k": "ノ",
+		"r": "no"
+	},
+	{
+		"h": "は",
+		"k": "ハ",
+		"r": "ha"
+	},
+	{
+		"h": "ひ",
+		"k": "ヒ",
+		"r": "hi"
+	},
+	{
+		"h": "ふ",
+		"k": "フ",
+		"r": "fu"
+	},
+	{
+		"h": "へ",
+		"k": "ヘ",
+		"r": "he"
+	},
+	{
+		"h": "ほ",
+		"k": "ホ",
+		"r": "ho"
+	},
+	{
+		"h": "ま",
+		"k": "マ",
+		"r": "ma"
+	},
+	{
+		"h": "み",
+		"k": "ミ",
+		"r": "mi"
+	},
+	{
+		"h": "む",
+		"k": "ム",
+		"r": "mu"
+	},
+	{
+		"h": "め",
+		"k": "メ",
+		"r": "me"
+	},
+	{
+		"h": "も",
+		"k": "モ",
+		"r": "mo"
+	},
+	{
+		"h": "や",
+		"k": "ヤ",
+		"r": "ya"
+	},
+	{
+		"h": "ゆ",
+		"k": "ユ",
+		"r": "yu"
+	},
+	{
+		"h": "よ",
+		"k": "ヨ",
+		"r": "yo"
+	},
+	{
+		"h": "ら",
+		"k": "ラ",
+		"r": "ra"
+	},
+	{
+		"h": "り",
+		"k": "リ",
+		"r": "ri"
+	},
+	{
+		"h": "る",
+		"k": "ル",
+		"r": "ru"
+	},
+	{
+		"h": "れ",
+		"k": "レ",
+		"r": "re"
+	},
+	{
+		"h": "ろ",
+		"k": "ロ",
+		"r": "ro"
+	},
+	{
+		"h": "わ",
+		"k": "ワ",
+		"r": "wa"
+	},
+	{
+		"h": "を",
+		"k": "ヲ",
+		"r": "wo"
+	},
+	{
+		"h": "ん",
+		"k": "ン",
+		"r": "n"
+	}
+]
+
+const all = []
+items.forEach((i) => {all.push(["h", i.h]); all.push(["k", i.k]); all.push(["r", i.r])})
+let got_right = []
+
+const settings = {
+	character: {
+		hiragana: true,
+		katakana: true,
+		romaji: false,
+	},
+	typeas: {
+		hiragana: false,
+		katakana: false,
+		romaji: true,
+	}
+}
+
 $(function() {
 	$("#answer_field").on("keyup", event => {
-		if (event.key !== "Enter") {return} // The only key we care about is Enter
-		$("#correct_answer").css("visibility") === "visible" ? $("#next_btn").trigger("click") : $("#answer_btn").trigger("click")
+		if (event.key === "Enter") {
+			$("#correct_answer").css("visibility") === "visible" ? $("#next_btn").trigger("click") : $("#answer_btn").trigger("click")
+		}
 	})
 
-	changeQuestion()
+	settings.character.hiragana = $("#hq").is(":checked")
+	$("#hq").change(() => {settings.character.hiragana = $("#hq").is(":checked");changeQuestion()})
+	settings.character.katakana = $("#kq").is(":checked")
+	$("#kq").change(() => {settings.character.katakana = $("#kq").is(":checked"); changeQuestion()})
+	settings.character.romaji = $("#rq").is(":checked")
+	$("#rq").change(() => {settings.character.romaji = $("#rq").is(":checked"); changeQuestion()})
+
+	settings.typeas.hiragana = $("#he").is(":checked")
+	$("#he").change(() => {settings.typeas.hiragana = $("#he").is(":checked"); changeQuestion()})
+	settings.typeas.katakana = $("#ke").is(":checked")
+	$("#ke").change(() => {settings.typeas.katakana = $("#ke").is(":checked"); changeQuestion()})
+	settings.typeas.romaji = $("#re").is(":checked")
+	$("#re").change(() => {settings.typeas.romaji = $("#re").is(":checked"); changeQuestion()})
+
 	$("#answer_field").on("input propertychange", () => {checkCharacters()})
 	$("#answer_field").on("focus", () => {$("#answer_field").select()})
 	$("#answer_btn").click(checkAnswer)
-	$("#next_btn").click(changeQuestion)
+	$("#next_btn").click(() => changeQuestion())
+
+	changeQuestion()
 })
 
-function changeQuestion() {
+function crash(are_we_looping) {
+	if (!are_we_looping) {
+		got_right = []
+		return changeQuestion(true)
+	} else {
+		$("#settings").css("opacity", "0")
+		$("#settings").animate({"opacity": "1"}, 200)
+		console.error("Impossible to ask anything with such settings!")
+		return
+	}
+}
+
+function changeQuestion(are_we_looping) {
+	const all_possibilities = all.filter((p) => !got_right.map((t) => t[1]).includes(p[1]))
+
+	const only_type_as_hiragana = settings.typeas.hiragana && !settings.typeas.katakana && !settings.typeas.romaji
+	const only_type_as_katakana = !settings.typeas.hiragana && settings.typeas.katakana && !settings.typeas.romaji
+	const only_type_as_romaji = !settings.typeas.hiragana && !settings.typeas.katakana && settings.typeas.romaji
+
+	const possibilities = all_possibilities.filter((p) => {
+		if (p[0] === "h" && settings.character.hiragana && !only_type_as_hiragana) {return p}
+		if (p[0] === "k" && settings.character.katakana && !only_type_as_katakana) {return p}
+		if (p[0] === "r" && settings.character.romaji && !only_type_as_romaji) {return p}
+	})
+
+	if (!possibilities.length) {
+		return crash(are_we_looping)
+	}
+
+	const i1 = Math.floor(Math.random() * possibilities.length)
+	const character = possibilities[i1][1]
+	const character_type = possibilities[i1][0]
+	const item = items.find((i) => i[character_type] === character)
+
+	const equivalent_types = []
+	if (settings.typeas.hiragana) {equivalent_types.push("h")}
+	if (settings.typeas.katakana) {equivalent_types.push("k")}
+	if (settings.typeas.romaji) {equivalent_types.push("r")}
+
+	const equivalents = equivalent_types
+		.filter((t) => t !== character_type)
+		.map((t) => [t, item[t]])
+
+	if (!equivalents.length) {
+		return crash(are_we_looping)
+	}
+
+	const i2 = Math.floor(Math.random() * equivalents.length)
+	const equivalent = equivalents[i2][1]
+	const equivalent_type = equivalents[i2][0]
+
 	$("#answer_field").val("") // Empty field so user doesn't have to do it
 	$("#answer_field").focus() // Focus on field so user doesn't have to do it
 	$("#answer_btn").attr("disabled", false)
@@ -19,64 +335,15 @@ function changeQuestion() {
 	$("#next_btn").css("display", "none")
 	$("#next_btn").html("skip")
 
-	let questionShape = shapeQuestion(
-		[$("#hq:checked").length, $("#kq:checked").length, $("#rq:checked").length],
-		[$("#he:checked").length, $("#ke:checked").length, $("#re:checked").length]
-	)
-	
-	if (!questionShape) { // No question can be found if settings are messed up
-		$("#settings").css("opacity", "0")
-		$("#settings").animate({"opacity": "1"}, 200)
-		return
-	}
-
-	changeBackground(questionShape[0], $("#character"))
-	changeBackground(questionShape[1], $("#equivalent"))
+	changeBackground(character_type, $("#character"))
+	changeBackground(equivalent_type, $("#equivalent"))
 
 	$("#correct_answer").css("visibility", "hidden")
 	$("#correct_answer").slideUp(100)
 
-	$.getJSON("characters.json", function(data) {
-		const characters = data.items[Math.floor(Math.random() * data.items.length)]
-		var char_in_box
-		var equivalent_of_char
-		var answer
-
-		switch (questionShape[0]) {
-			case "h":
-				char_in_box = characters.h
-				break
-			case "k":
-				char_in_box = characters.k
-				break
-			case "r":
-				char_in_box = characters.r
-				break
-			default:
-				char_in_box = "???"
-		}
-
-		switch (questionShape[1]) {
-			case "h":
-				equivalent_of_char = "ひらがな"
-				answer = characters.h
-				break
-			case "k":
-				equivalent_of_char = "カタカナ"
-				answer = characters.k
-				break
-			case "r":
-				equivalent_of_char = "rōmaji"
-				answer = characters.r
-				break
-			default:
-				equivalent_of_char = "???"
-		}
-
-		$("#character").html(char_in_box)
-		$("#equivalent").html(equivalent_of_char)
-		$("#answer_paragraph").html(`the answer was ${answer}`)
-	})
+	$("#character").html(character)
+	$("#equivalent").html(equivalent_type === "h" ? "ひらがな" : equivalent_type === "k" ? "カタカナ" : "rōmaji")
+	$("#answer_paragraph").html(`the answer was ${equivalent}`)
 }
 
 function changeBackground(char, para) {
@@ -86,39 +353,13 @@ function changeBackground(char, para) {
 	else if (char === "r") {colour = "blueviolet"}
 
 	para.css("backgroundColor", colour)
-}
-
-function shapeQuestion(question, equivalent) {
-	let possibilities = ["h", "k", "r"]
-	var element_one
-	var element_two
-
-	question = question.map((a, i) => {if (a) return possibilities[i]}).filter((a) => a)
-	equivalent = equivalent.map((a, i) => {if (a) return possibilities[i]}).filter((a) => a)
-	if (question.length < 1 || equivalent.length < 1) {return false}
-	if (question.length === 1 && equivalent.length === 1) {
-		if (JSON.stringify(question) === JSON.stringify(equivalent)) {return false}
-	}
-
-	while (element_one === undefined || element_two === undefined) {
-		let x = Math.floor(Math.random() * question.length)
-		if (question[x]) {
-			element_one = question[x]
-		}
-
-		let y = Math.floor(Math.random() * equivalent.length)
-		if (equivalent[y] && equivalent[y] != element_one) {
-			element_two = equivalent[y]
-		}
-	}
-
-	return [element_one, element_two]
+	para.attr("char", char)
 }
 
 function checkCharacters() {
 	const REGEX_JAPANESE = /[\u3040-\u30ff]/
 	const REGEX_OTHER = /[^\u3040-\u30ff]/
-	let regex = $("#equivalent").html() === "rōmaji" ? REGEX_JAPANESE : REGEX_OTHER
+	const regex = $("#equivalent").html() === "rōmaji" ? REGEX_JAPANESE : REGEX_OTHER
 
 	if (!$("#answer_field").val() || !$("#answer_field").val().match(regex)) {
 		$("#answer_field").css("backgroundColor", "black")
@@ -136,8 +377,11 @@ function checkAnswer() {
 		return
 	}
 
-	let html = $("#answer_paragraph").html()
-	let element = $("#answer_field").val().toLowerCase() === html.slice(html.lastIndexOf(" ") + 1) ? $("#positive") : $("#negative")
+	const html = $("#answer_paragraph").html()
+	const correct = $("#answer_field").val().toLowerCase() === html.slice(html.lastIndexOf(" ") + 1)
+	const element = correct ? $("#positive") : $("#negative")
+	if (correct) {got_right.push([$("#character").attr("char"), $("#character").html()])}
+
 	element.css("opacity", "0")
 	element.css("backgroundColor", element.css("color"))
 	setTimeout(() => {element.css("backgroundColor", "black")}, 125)
